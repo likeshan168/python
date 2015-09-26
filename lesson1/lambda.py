54c63465e32042480110e957241c9489ef94d99c
likeshan168@gmail.com <likeshan168@gmail.com>
2015-09-26 08:56:04 +0800

    lambda expression

---
+++ lesson1/lambda.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# filename lambda.py
+
+#def make_incrementor(n):
+#	return lambda x: x+n
+
+#print make_incrementor(43)
+
+f = lambda a, b, c:a+b+c
+
+print f(1,2,3)
